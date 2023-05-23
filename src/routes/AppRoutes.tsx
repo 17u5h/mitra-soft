d4d72aad84ca5d4d77aa170ccea2696890b64c39
17u5h <102058870+17u5h@users.noreply.github.com>
2023-05-23 15:44:19 +0700

    pages routing added

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Main from "../pages/Main";
+import NotFound from "../pages/NotFound";
+import UserProfile from "../pages/UserProfile";
+import MyProfile from "../pages/MyProfile";
+
+const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Main/>}/>
+			<Route path="/my-profile" element={<MyProfile/>}/>
+			<Route path="/user-profile/:id" element={<UserProfile/>}/>
+
+			<Route path="*" element={<NotFound/>}/>
+		</Routes>
+	);
+};
+
+export default AppRoutes;
