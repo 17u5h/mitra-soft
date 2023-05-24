6a5ec09d1bbd9d818e1702e2a157c65f02a3dc94
17u5h <102058870+17u5h@users.noreply.github.com>
2023-05-25 00:26:43 +0700

    UI button and logic "go to main" added

---
+++ src/components/UI/UIButton/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.css'
+
+
+type Props = {
+	children: string
+	onClick: () => void
+}
+
+const UIButton = ({onClick, children}: Props) => {
+	return (
+		<button onClick={onClick} className={styles.button}>
+			{children}
+		</button>
+	);
+};
+
+export default UIButton;
--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -9,49 +9,57 @@ import styles from './styles.module.css'
 import {Post} from "../../types/Post";
 import Posts from "../../components/Posts";
 import emptyAvatar from "../../assets/emptyAvatar.png";
+import UIButton from "../../components/UI/UIButton";
 
 
 const UserProfile = () => {
-	const [foundUser, setFoundUser] = useState<User>()
-	const [userPosts, setUserPosts] = useState<Post[]>()
-	const {userId} = useParams()
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	const users = useSelector((state: RootState) => state.usersReducer.users)
-	const posts = useSelector((state: RootState) => state.postsReducer.posts)
-	const isLoading = useSelector((state: RootState) => state.usersReducer.isLoading)
+		const [foundUser, setFoundUser] = useState<User>()
+		const [userPosts, setUserPosts] = useState<Post[]>()
+		const {userId} = useParams()
+		const dispatch = useDispatch()
+		const navigate = useNavigate()
+		const users = useSelector((state: RootState) => state.usersReducer.users)
+		const posts = useSelector((state: RootState) => state.postsReducer.posts)
+		const isLoading = useSelector((state: RootState) => state.usersReducer.isLoading)
 
-	useEffect(() => {
-		dispatch(fetchUsers())
-	}, [])
+		useEffect(() => {
+			dispatch(fetchUsers())
+		}, [])
 
-	useEffect(() => {
-		const foundUser = users.filter((user: User) => user.id === Number(userId))
-		const usersPosts = posts.filter((post: Post) => post.userId === Number(userId))
-		setFoundUser(foundUser[0])
-		setUserPosts(usersPosts)
-	}, [users])
+		useEffect(() => {
+			const foundUser = users.filter((user: User) => user.id === Number(userId))
+			const usersPosts = posts.filter((post: Post) => post.userId === Number(userId))
+			setFoundUser(foundUser[0])
+			setUserPosts(usersPosts)
+		}, [users])
 
 
-	return (
-		<div className={styles.container}>
-			{isLoading ?
-				<Spinner animation='border' variant='secondary'/> :
-				<>
-					<div className={styles.aboutUser}>
-						<img src={emptyAvatar} alt="user's photo" className={styles.userPhoto} onClick={() => navigate(`/user-profile/${userId}`)}/>
-						<div className={styles.userInfo}>
-							<p className={styles.profileName}>{foundUser?.name}</p>
-							<p>{foundUser?.email}</p>
-							<p>{foundUser?.website}</p>
+		const goBackward = () => {
+			navigate('/')
+		}
+
+		return (
+			<div className={styles.container}>
+				{isLoading ?
+					<Spinner animation='border' variant='secondary'/> :
+					<>
+						<div className={styles.aboutUser}>
+							<img src={emptyAvatar} alt="user's photo" className={styles.userPhoto}
+									 onClick={() => navigate(`/user-profile/${userId}`)}/>
+							<div className={styles.userInfo}>
+								<p className={styles.profileName}>{foundUser?.name}</p>
+								<p>{foundUser?.email}</p>
+								<p>{foundUser?.website}</p>
+							</div>
+							<UIButton onClick={goBackward}>Назад</UIButton>
 						</div>
-					</div>
-					<Posts posts={userPosts} isLoading={isLoading}/>
-
-				</>
-			}
-		</div>
-	);
-};
+						<Posts posts={userPosts} isLoading={isLoading}/>
+						<UIButton onClick={goBackward}>Назад</UIButton>
+					</>
+				}
+			</div>
+		);
+	}
+;
 
 export default UserProfile;
